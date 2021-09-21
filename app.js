95c15abd5a501c73713b91f7137d574d50fe487b
津田将信 <tsudashoushin@tsudashoushinnoMacBook-Air-2.local>
2021-09-21 23:58:00 +0900

    third commit

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-line = 1;
+//今日の目的
+//vscodeに慣れる　gitの使い方慣れる
+//再帰の練習
 
-console.log(line);
+function number(x,y){
+
+    let answer = 0;
+    let a = Math.abs(x);
+    let b = Math.abs(y);
+
+
+    for(let i = 1;i<=b;i++){
+        answer = answer +a;
+    }
+
+    if(y<0 && x>0||y>0&&x<0){
+        return answer*-1;
+    }else{
+        return answer;
+    }
+}
+
+console.log(number(5,4))
